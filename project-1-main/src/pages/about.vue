a79fdd7ff8fafdf691a0a883fda65a58794277d2
DarbanMM <darbanmaha@gmail.com>
2023-11-27 23:21:06 +0700

    first commit

---
+++ project-1-main/src/pages/about.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>Halaman Tentang</p>
+    <br>
+    <p>Halaman Tentang atau About Page adalah bagian penting dari sebuah situs web yang memberikan informasi detail tentang identitas, visi, misi, sejarah, dan nilai-nilai inti dari suatu entitas atau organisasi. Ini bisa berupa perusahaan, individu, organisasi nirlaba, atau proyek tertentu.</p>
+    <br>
+    <p>Di dalam About page, biasanya terdapat beberapa informasi kunci:</p>
+    <p>1. Identitas:</p>
+    <p>> Nama: Nama perusahaan atau individu.</p>
+    <p>> Gambaran umum: Deskripsi singkat tentang apa yang entitas tersebut lakukan, fokusnya, dan apa yang membuatnya unik.</p>
+    <p>> Kontak: Alamat, nomor telepon, alamat email, dan mungkin juga peta lokasi.</p>
+    <br>
+    <p>2. Sejarah:</p>
+    <p>> Asal Usul: Kisah singkat bagaimana entitas tersebut dimulai atau sejarah singkatnya.</p>
+    <p>> Perkembangan: Melewati milestone atau momen penting yang telah dicapai.</p>
+    <br>
+    <p>3. Visi dan Misi:</p>
+    <p>> Visi: Gambaran besar tentang tujuan jangka panjang atau arah yang ingin dicapai.</p>
+    <p>> Misi: Tujuan-tujuan spesifik yang mendukung visi tersebut.</p>
+    <br>
+    <p>4. Tim atau Individu:</p>
+    <p>> Informasi tentang anggota tim: Jika ini adalah perusahaan atau organisasi, informasi tentang orang-orang di baliknya.</p>
+    <p>> Pengalaman: Detail tentang latar belakang atau keahlian yang dimiliki oleh tim atau individu.</p>
+    <br>
+    <p>5. Penghargaan atau Prestasi:</p>
+    <p>> Pengakuan: Penghargaan, sertifikasi, atau prestasi yang telah dicapai oleh entitas tersebut.</p>
+    <br>
+    <p>6. Nilai Inti:</p>
+    <p>> Nilai dan Prinsip: Nilai-nilai inti atau prinsip-prinsip yang menjadi landasan dari entitas tersebut.</p>
+    <br>
+    <p>7. Pembaruan Terkini:</p>
+    <p>> Berita Terbaru: Pembaruan terbaru atau acara-acara yang relevan dengan entitas tersebut.</p>
+    <br>
+    <p>About page seringkali menjadi salah satu halaman yang paling sering dikunjungi oleh pengunjung situs web karena memberikan wawasan yang lebih mendalam tentang entitas atau individu di baliknya. Ini memungkinkan pengunjung untuk memahami lebih baik siapa mereka, apa yang mereka tawarkan, dan bagaimana mereka berbeda dari yang lain.</p>
+  </div>
+</template>
---
+++ project-1-main/src/pages/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>Halaman Beranda
+    <br>
+    <br>
+    <p>Halaman Beranda atau Home Page adalah halaman pertama atau halaman depan yang muncul ketika seseorang mengunjungi suatu situs web. Ini adalah titik awal yang memberikan gambaran umum tentang konten, tujuan, dan navigasi situs web tersebut.</p>
+    <br>
+    <P>Komponen-Komponen Utama dalam Home Page:</P>
+    <br>
+    <P>1. Navigasi:</P>
+    <P>> Menu dan Link: Memberikan navigasi yang jelas ke halaman-halaman penting lainnya di situs web.</P>
+    <P>> Footer: Link tambahan dan informasi penting lainnya.</P>
+    <br>
+    <P>2. Konten Informasi:</P>
+    <P>> Headline/Pembukaan: Biasanya berisi informasi singkat atau headline yang menarik perhatian pengunjung.</P>
+    <P>> Deskripsi Singkat: Penjelasan singkat tentang apa yang situs web tersebut tawarkan atau apa fokus utamanya.</P>
+    <P>> Fitur Unggulan: Mungkin ada sejumlah fitur utama, produk, atau layanan yang ditampilkan secara visual atau dalam bentuk ringkasan.</P>
+    <br>
+    <P>3. Call-to-Action (CTA):</P>
+    <P>> Tombol atau Tautan Tindakan: Mengarahkan pengunjung untuk melakukan tindakan tertentu, seperti mendaftar, membeli, atau menjelajahi lebih lanjut.</P>
+    <br>
+    <P>4. Elemen Visual:</P>
+    <P>> Gambar atau Video: Visual menarik yang mencerminkan merek atau isi situs web.</P>
+    <P>> Desain Responsif: Didesain agar responsif di berbagai perangkat, baik komputer maupun perangkat mobile.</P>
+    <br>
+    <P>5. Pembaruan Terkini:</P>
+    <P>> Konten Terbaru: Beberapa situs web memasukkan bagian dengan konten terbaru atau pembaruan terkini di home page untuk menjaga pengunjung tetap terinformasi.</P>
+    <br>
+    <P>Tujuan Home Page:</P>
+    <P>Home page bertujuan untuk memberikan pengantar yang baik kepada pengunjung tentang apa yang bisa mereka harapkan dari situs web tersebut. Ini juga berfungsi sebagai pusat navigasi, membantu pengunjung menemukan informasi lebih lanjut tentang topik, produk, atau layanan yang mereka cari.</P>
+    <br>
+    <P>Home page biasanya merupakan titik masuk yang penting bagi pengunjung, oleh karena itu, desainnya sering diperhatikan dengan seksama untuk memastikan kesan pertama yang baik, navigasi yang intuitif, dan ketersediaan informasi yang dibutuhkan.</P>
+    <br>
+    <P>Selain itu, peran dari home page juga bisa berbeda tergantung pada jenis situs webnya. Misalnya, situs e-commerce akan menampilkan produk unggulan, sementara situs berita akan menampilkan berita terbaru. Adaptasi terhadap kebutuhan dan tujuan situs web tertentu sangat penting dalam merancang home page yang efektif.</P>
+  </div>
+</template>
